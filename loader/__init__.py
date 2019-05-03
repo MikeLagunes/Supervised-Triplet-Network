59b4a03d7bae4c265ffbba12513071857444b14d
MikeLagunes <malfkov@gmail.com>
2019-05-03 12:58:43 +0100

    clean code

--- loader/__init__.py
+++ loader/__init__.py
@@ -1,17 +1,5 @@
 import json
 
-from loader.cnn_tless import cnn_tless
-from loader.triplet_resnet_tless import triplet_resnet_tless
-from loader.triplet_resnet_tless_softmax import triplet_resnet_tless_softmax
-
-from loader.cnn_toybox import cnn_toybox
-from loader.triplet_resnet_toybox import triplet_resnet_toybox
-from loader.triplet_resnet_toybox_softmax import triplet_resnet_toybox_softmax
-
-from loader.cnn_arc import cnn_arc
-from loader.triplet_resnet_arc import triplet_resnet_arc
-from loader.triplet_resnet_arc_softmax import triplet_resnet_arc_softmax
-
 from loader.cnn_core50 import cnn_core50
 from loader.triplet_resnet_core50 import triplet_resnet_core50
 from loader.triplet_resnet_core50_softmax import triplet_resnet_core50_softmax
@@ -24,18 +12,7 @@ def get_loader(name):
     :param name:
     """
     return {
-        'cnn_tless':cnn_tless,
-        'triplet_resnet_tless': triplet_resnet_tless,
-        'triplet_resnet_tless_softmax': triplet_resnet_tless_softmax,
-
-        'cnn_toybox':cnn_toybox,
-        'triplet_resnet_toybox': triplet_resnet_toybox,
-        'triplet_resnet_toybox_softmax': triplet_resnet_toybox_softmax,
-         
-        'cnn_arc':cnn_arc,
-        'triplet_resnet_arc': triplet_resnet_arc,
-        'triplet_resnet_arc_softmax': triplet_resnet_arc_softmax,
-        
+      
         'cnn_core50':cnn_core50,
         'triplet_resnet_core50': triplet_resnet_core50,
         'triplet_resnet_core50_softmax': triplet_resnet_core50_softmax,
---
+++ models/__init__.py
@@ -0,0 +1,3 @@
+from cnn_resnet import *
+from triplet_resnet import *
+from triplet_resnet_softmax import *
